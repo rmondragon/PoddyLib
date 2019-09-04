1ea6386dfbe501cf3715a257e397b55405cc5a46
Rolando Galindo <rmondragon@gmail.com>
2019-09-04 14:47:15 -0400

    Added test cases

---
+++ PoddyLib/PoddyLib.swift
@@ -0,0 +1,21 @@
+//
+//  PoddyLib.swift
+//  PoddyLib
+//
+//  Created by Rolando Galindo on 9/4/19.
+//  Copyright © 2019 Rolando Galindo. All rights reserved.
+//
+
+public final class PoddyLib {
+  
+  let name = "PoddyLib"
+  
+  public func add(a: Int, b: Int) -> Int {
+    return a + b
+  }
+  
+  public func sub(a: Int, b: Int) -> Int {
+    return a - b
+  }
+  
+}
--- PoddyLibTests/PoddyLibTests.swift
+++ PoddyLibTests/PoddyLibTests.swift
@@ -11,24 +11,17 @@ import XCTest
 
 class PoddyLibTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  var poddyLib: PoddyLib!
+  
+  override func setUp() {
+    poddyLib = PoddyLib()
+  }
+  
+  func testAdd() {
+    XCTAssertEqual(poddyLib.add(a: 1, b: 1), 2)
+  }
+  
+  func testSub() {
+    XCTAssertEqual(poddyLib.sub(a: 2, b: 1), 1)
+  }
 }
