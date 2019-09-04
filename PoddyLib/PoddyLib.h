7d0d685d69e84dfe7512b469bc8c47af115d7564
Rolando Galindo <rmondragon@gmail.com>
2019-09-04 12:51:48 -0400

    Initial Commit

---
+++ PoddyLib/PoddyLib.h
@@ -0,0 +1,19 @@
+//
+//  PoddyLib.h
+//  PoddyLib
+//
+//  Created by Rolando Galindo on 9/4/19.
+//  Copyright © 2019 Rolando Galindo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PoddyLib.
+FOUNDATION_EXPORT double PoddyLibVersionNumber;
+
+//! Project version string for PoddyLib.
+FOUNDATION_EXPORT const unsigned char PoddyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PoddyLib/PublicHeader.h>
+
+
